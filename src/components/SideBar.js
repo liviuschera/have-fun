d531e43bc0310ab6444a554b6dbc892917e50c2d
Liviu Schera <liviu115@gmail.com>
2018-08-04 13:54:17 +0930

    mend

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { Subtitle, Paragraph } from "./typography";
+import { Subtitle, Paragraph, Remark } from "./typography";
+import { inputSelectField } from "../components/sharedStyles";
 
 const SidebarAside = styled.aside`
    display: grid;
@@ -11,18 +12,39 @@ const SidebarAside = styled.aside`
    margin-left: 4rem;
    background-color: var(--color-grey-light-2);
 `;
+0;
 
 const LocationArticle = styled.article`
    height: 13rem;
    padding: var(--sidebar-article-padding);
    border-bottom: 1px solid var(--color-grey-light-3);
+   select {
+      ${inputSelectField};
+      width: 100%;
+   }
 `;
 
 const DateArticle = styled.article`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   /* grid-template-rows: repeat(3, 1fr); */
    height: 19rem;
    padding: var(--sidebar-article-padding);
-
    border-bottom: 1px solid var(--color-grey-light-3);
+   align-content: center;
+   grid-gap: 0.5rem;
+
+   input {
+      ${inputSelectField};
+      width: max-content;
+
+      /* height: 4rem;
+      width: 17rem;
+      font-family: Roboto;
+      font-size: var(--font-paragraph-size); */
+      /* line-height: var(--font-paragraph-size); */
+      /* align-self: center; */
+   }
 `;
 
 const CategoriesArticle = styled.article`
@@ -30,13 +52,45 @@ const CategoriesArticle = styled.article`
    padding: var(--sidebar-article-padding);
 `;
 
+// const Input = styled.input`
+//    height: 4rem;
+//    width: 17rem;
+//    font-family: Roboto;
+//    /* font-size: var(--font-paragraph-size); */
+//    /* line-height: var(--font-paragraph-size); */
+//    align-self: center;
+
+//    /* padding-left: 1rem; */
+// `;
+const Span = styled.span`
+   display: grid;
+
+   box-decoration-break: clone;
+   justify-self: flex-end;
+   text-align: end;
+`;
+
+const SBSubtitle = styled(Subtitle)`
+   grid-column: 1/-1;
+`;
+
 const SideBar = props => (
    <SidebarAside>
       <LocationArticle>
          <Subtitle>Location</Subtitle>
+         <select>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+         </select>
       </LocationArticle>
       <DateArticle>
-         <Subtitle>Date</Subtitle>
+         <SBSubtitle>Date</SBSubtitle>
+         <span>from</span>
+         <input type="date" value="1980-08-26" />
+         <span>to</span>
+         <input type="date" value="1980-08-26" />
       </DateArticle>
       <CategoriesArticle>
          <Subtitle>Categories</Subtitle>
---
+++ src/components/sharedStyles.js
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+export const inputSelectField = css`
+   height: 4rem;
+   font-family: Roboto;
+   font-size: var(--font-paragraph-size);
+   border: none;
+   padding-left: 0.5rem;
+`;
